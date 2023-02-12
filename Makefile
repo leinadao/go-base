75fe0215e0dcb11e9464f21c135d2e628a3a31da
Daniel Osborne <danaosb@gmail.com>
2023-02-12 16:52:59 +0000

    Add make test Makefile command for running unit tests.

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
-.PHONY: version deps
+# Confluent Kafka Go package tags:
+GO_CKG_TAGS ?= dynamic
+
+.PHONY: version deps test
 
 version: ## Determine and output current version.
 	@[[ -z "${VERSION}" ]] && git describe --tags --always | sed 's/v//' || echo $(VERSION)
 
 deps: ## Sync dependencies.
 	go mod tidy
+
+test: ## Run all unit tests.
+	go test -tags unit,$(GO_CKG_TAGS) -count=1 -coverprofile coverage.out -covermode count -coverpkg=$(shell go list ./... | tr '\n' ',') -v ./...
--- makefile_test.go
+++ makefile_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"os"
 	"os/exec"
 	"testing"
 
@@ -11,91 +12,192 @@ import (
 func TestMakefile(t *testing.T) {
 	// Not suitable for running parallel due to environment variable modification.
 
-	type testCase struct {
-		setup         func(*testing.T)
-		withEnv       map[string]string
-		executable    string
-		parameters    []string
-		outputChecker func(*testing.T, string)
-		cleanup       func(*testing.T)
-	}
+	// envNameTestMakefileRunning is the name of an environment variable set when this
+	// test is running which will prevent the test from recursively running whilst
+	// testing the makefile commands.
+	const envNameTestMakefileRunning = "TEST_MAKEFILE_RUNNING"
 
-	testCases := map[string]testCase{
-		"version returns the current git describe tag if no VERSION environment variable is set": {
-			executable: "make",
-			parameters: []string{"version"},
-			outputChecker: func(t *testing.T, got string) {
-				currentGitTag, err := exec.Command("git", "describe", "--tags", "--always").Output()
-				assert.NoError(t, err, "Checking current git tag")
-				assert.Equal(t, string(currentGitTag), got, "'make version' output")
-			},
-		},
-		"version returns the VERSION environment variable when set": {
-			withEnv: map[string]string{
-				"VERSION": "some-version",
-			},
-			executable: "make",
-			parameters: []string{"version"},
-			outputChecker: func(t *testing.T, got string) {
-				assert.Equal(t, "some-version\n", got, "'make version' output")
-			},
-		},
-		"version returns git tag with v prefix stripped when in git tag": {
-			setup: func(t *testing.T) {
-				out, err := exec.Command("git", "tag", "v9999.9.9").Output()
-				assert.NoError(t, err, "Creating temp git tag - error")
-				assert.Empty(t, out, "Creating temp git tag - output")
-			},
-			executable: "make",
-			parameters: []string{"version"},
-			outputChecker: func(t *testing.T, got string) {
-				assert.Equal(t, "9999.9.9\n", got, "'make version' output")
-			},
-			cleanup: func(t *testing.T) {
-				out, err := exec.Command("git", "tag", "-d", "v9999.9.9").Output()
-				assert.NoError(t, err, "Clean up temp git tag - error")
-				assert.Contains(t, string(out), "Deleted tag 'v9999.9.9' (was", "Clean up temp git tag - output")
-			},
-		},
-		"version returns environment variable VERSION with v prefix is not stripped from VERSION environment variable when set": {
-			withEnv: map[string]string{
-				"VERSION": "v3.2.1",
-			},
-			executable: "make",
-			parameters: []string{"version"},
-			outputChecker: func(t *testing.T, got string) {
-				assert.Equal(t, "v3.2.1\n", got, "'make version' output")
-			},
-		},
-		"deps runs a go mod tidy": {
-			executable: "make",
-			parameters: []string{"deps"},
-			outputChecker: func(t *testing.T, got string) {
-				assert.Equal(t, "go mod tidy\n", got, "'make deps' output")
-			},
-		},
-	}
+	if _, set := os.LookupEnv(envNameTestMakefileRunning); set != true {
 
-	for tName, tCase := range testCases {
-		tn, tc := tName, tCase
+		type testCase struct {
+			setup         func(*testing.T)
+			withEnv       map[string]string
+			executable    string
+			parameters    []string
+			outputChecker func(*testing.T, string)
+			cleanup       func(*testing.T)
+		}
 
-		t.Run(tn, func(t *testing.T) {
-			// Not suitable for running parallel due to environment variable modification.
-			if tc.setup != nil {
-				tc.setup(t)
-			}
+		testCases := map[string]testCase{
+			"version returns the current git describe tag if no VERSION environment variable is set": {
+				executable: "make",
+				parameters: []string{"version"},
+				outputChecker: func(t *testing.T, got string) {
+					currentGitTag, err := exec.Command("git", "describe", "--tags", "--always").Output()
+					assert.NoError(t, err, "Checking current git tag")
+					assert.Equal(t, string(currentGitTag), got, "'make version' output")
+				},
+			},
+			"version returns the VERSION environment variable when set": {
+				withEnv: map[string]string{
+					"VERSION": "some-version",
+				},
+				executable: "make",
+				parameters: []string{"version"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Equal(t, "some-version\n", got, "'make version' output")
+				},
+			},
+			"version returns git tag with v prefix stripped when in git tag": {
+				setup: func(t *testing.T) {
+					out, err := exec.Command("git", "tag", "v9999.9.9").Output()
+					assert.NoError(t, err, "Creating temp git tag - error")
+					assert.Empty(t, out, "Creating temp git tag - output")
+				},
+				executable: "make",
+				parameters: []string{"version"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Equal(t, "9999.9.9\n", got, "'make version' output")
+				},
+				cleanup: func(t *testing.T) {
+					out, err := exec.Command("git", "tag", "-d", "v9999.9.9").Output()
+					assert.NoError(t, err, "Clean up temp git tag - error")
+					assert.Contains(t, string(out), "Deleted tag 'v9999.9.9' (was", "Clean up temp git tag - output")
+				},
+			},
+			"version returns environment variable VERSION with v prefix is not stripped from VERSION environment variable when set": {
+				withEnv: map[string]string{
+					"VERSION": "v3.2.1",
+				},
+				executable: "make",
+				parameters: []string{"version"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Equal(t, "v3.2.1\n", got, "'make version' output")
+				},
+			},
+			"deps runs a go mod tidy command": {
+				executable: "make",
+				parameters: []string{"deps"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Equal(t, "go mod tidy\n", got, "'make deps' output")
+				},
+			},
+			"test runs a go test command": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Regexp(t, "go test.", got, "'make test' output")
+				},
+			},
+			"test uses dynamic tag to support Confluent kafka go use on multiple devices": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Regexp(t, "-tags [^-]*dynamic", got, "'make test' output")
+				},
+			},
+			"test uses unit tag to run only unit tests": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Regexp(t, "-tags [^-]*unit", got, "'make test' output")
+				},
+			},
+			"test uses a non-cacheable count flag so that no test caching occurs": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Regexp(t, "-count=[0-9]*", got, "'make test' output")
+				},
+			},
+			"test uses coverage.out value for coverprofile flag": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Contains(t, got, "-coverprofile coverage.out", "'make test' output")
+				},
+			},
+			"test uses count value for covermode flag": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Contains(t, got, "-covermode count", "'make test' output")
+				},
+			},
+			"test uses comma separated go list output for coverpkg flag": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					goListCSV, err := exec.Command("bash", "-c", "go list ./... | tr '\n' ','").Output()
+					assert.NoError(t, err, "Checking go list expected output - error")
 
-			for k, v := range tc.withEnv {
-				t.Setenv(k, v)
-			}
+					assert.Contains(t, got, "-coverpkg="+string(goListCSV), "'make test' output")
+				},
+			},
+			"test uses verbose output flag": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Contains(t, got, "-v", "'make test' output")
+				},
+			},
+			"test runs for all paths and subpaths": {
+				withEnv: map[string]string{
+					envNameTestMakefileRunning: "true",
+				},
+				executable: "make",
+				parameters: []string{"test"},
+				outputChecker: func(t *testing.T, got string) {
+					assert.Regexp(t, `\Q./...\E`, got, "'make test' output")
+				},
+			},
+		}
 
-			got, err := exec.Command(tc.executable, tc.parameters...).Output()
-			assert.NoError(t, err)
-			tc.outputChecker(t, string(got))
+		for tName, tCase := range testCases {
+			tn, tc := tName, tCase
 
-			if tc.cleanup != nil {
-				tc.cleanup(t)
-			}
-		})
+			t.Run(tn, func(t *testing.T) {
+				// Not suitable for running parallel due to environment variable modification.
+				if tc.setup != nil {
+					tc.setup(t)
+				}
+
+				for k, v := range tc.withEnv {
+					t.Setenv(k, v)
+				}
+
+				got, err := exec.Command(tc.executable, tc.parameters...).Output()
+				assert.NoError(t, err)
+				tc.outputChecker(t, string(got))
+
+				if tc.cleanup != nil {
+					tc.cleanup(t)
+				}
+			})
+		}
 	}
 }
